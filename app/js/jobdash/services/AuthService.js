025317ff1d5e24a9ca2496ae2760311aec5178e6
Dustin McBride <dustinbm@gmail.com>
2016-07-11 15:29:45 -0700

    added drew style auth

--- app/js/jobdash/services/AuthService.js
+++ app/js/jobdash/services/AuthService.js
@@ -1,36 +1,43 @@
-'use strict';
-module.exports = function(app){
-  app.factory('AuthService', function($http){
-    let token;
+module.exports = function(app) {
+  app.factory('AuthService', function($http, $window) {
+    let token = $window.localStorage.token;
     const service = {};
 
-    service.signUp = function(user){
+    service.signUp = function(user) {
       return $http.post('http://localhost:3000/signup', user)
-      .then((res) => {
-        token = res.data.token;
-        return res;
-      });
+        .then((res) => {
+          token = res.data.token;
+          $window.localStorage.token = token;
+          return res;
+        });
     };
-    service.signIn = function(user){
-      console.log('in service signin', user);
-      let base64 = btoa(user.username + ':' + user.password);
-      let authString = 'Basic ' + base64;
+
+    service.signIn = function(user) {
+      let base64Auth = btoa(user.username + ':' + user.password);
+      let authString = 'Basic ' + base64Auth;
+
       return $http({
-        method: 'POST',
-        data:user,
         url: 'http://localhost:3000/signin',
+        methods: 'POST',
         headers: {
           authorization: authString
         }
-      })
-      .then((res) => {
+      }).then((res) => {
+        console.log('here');
         token = res.data.token;
+        $window.localStorage.token = token;
         return res;
       });
     };
-    service.getToken = function(){
+
+    service.signOut = function() {
+      token = $window.localStorage.token = null;
+    };
+
+    service.getToken = function() {
       return token;
     };
+
     return service;
   });
 };
