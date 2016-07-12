9c01145ece575af6e88666ae9a40a74911d40f64
Dustin McBride <dustinbm@gmail.com>
2016-07-12 13:18:11 -0700

    list now save

--- app/js/jobdash/directives/jobList.js
+++ app/js/jobdash/directives/jobList.js
@@ -1,31 +1,54 @@
 module.exports = function (app) {
+
+  let updateListItem = function (newList, oldList) {
+
+    var bIds = {};
+    oldList.forEach(function (obj) {
+      bIds[obj._id] = obj;
+    });
+
+    // Return all elements in A, unless in B
+    let delta = newList.filter(function (obj) {
+      return !(obj._id in bIds);
+    });
+    return delta;
+  };
+
   app.directive('jobList', function () {
-    var controller = ['$scope', function ($scope) {
-
-      $scope.logEvent = function (message, event) {
-        console.log(message, '(triggered by the following', event.type, 'event)');
-        console.log(event);
-      };
-
-      $scope.$watch('today', function (model) {
-        console.log(model);
-        console.log('yeah');
-      }, true);
-
-      $scope.$watch('jobs', function (model) {
-        console.log(model);
-        console.log('Oh, yeah!');
-      }, true);
-
-    }];
     return {
       templateUrl: './templates/job/joblist.html',
       scope: {
-        jobs: '=',
+        backlog: '=',
         today: '=',
         form: '@'
       },
-      controller: controller
+      require: '^^ngController',
+      link: function ($scope, elem, attr, controller) {
+        $scope.$watch('today', function (newModel, oldModel) {
+          console.log('today', $scope.today, $scope.today.length);
+          console.log();
+          let delta = updateListItem(newModel, oldModel);
+          if (delta.length == 1) {
+            delta[0].isToday = true;
+            controller.updateJobs(delta[0]);
+          //   let index = $scope.today.indexOf(delta[0]);
+          //   console.log(index);
+          //   $scope.today.splice(index, 1);
+           }
+        }, true);
+
+        $scope.$watch('backlog', function (newModel, oldModel) {
+          console.log('bl', $scope.backlog, $scope.backlog.length );
+          let delta = updateListItem(newModel, oldModel);
+          if (delta.length == 1) {
+            delta[0].isToday = false;
+            controller.updateJobs(delta[0]);
+            // let index = $scope.today.indexOf(delta[0]);
+            // console.log(index);
+            // $scope.today.splice(index, 1);
+          }
+        }, true);
+      }
     };
   });
 };
