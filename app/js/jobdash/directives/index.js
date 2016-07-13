33897d11a94644b86313d0480a877af343965e04
jdar22169 <mmow313@gmail.com>
2016-07-13 11:50:03 -0700

    single click now working

--- app/js/jobdash/directives/index.js
+++ app/js/jobdash/directives/index.js
@@ -1,8 +1,6 @@
 module.exports = function(app) {
   require('./backlog')(app);
   require('./jobForm')(app);
-  require('./eventList')(app);
-  require('./jobItem')(app);
   require('./pane')(app);
   require('./paneContainer')(app);
   require('./addbar')(app);
