ea0b7f6328e6c46201d94f33cff0acd14b349d55
jdar22169 <mmow313@gmail.com>
2016-07-14 13:24:04 -0700

    testing for job controller done

--- test/jobcontroller_test.js
+++ test/jobcontroller_test.js
@@ -10,7 +10,7 @@ describe('JobController Testing', () => {
 
   beforeEach(() => {
     angular.mock.module('JobDash');
-    angular.mock.inject(function($controller, _$httpBackend_, sortJobs){
+    angular.mock.inject(function($controller, _$httpBackend_){
       jobctrl = new $controller('JobController');
       $httpBackend = _$httpBackend_;
     });
@@ -33,12 +33,52 @@ describe('JobController Testing', () => {
     expect(jobctrl.events[0].note).toBe('testEvent');
   });
 
-  it('should post a job', () => {
+  it('should attach to linkApiJob object', () => {
     $httpBackend.expectPOST('http://localhost:3000/link')
-    .respond(200, {title:'testing', company:'123'});
+    .respond(200, {title:'testing', company:'123', url: 'www.test.com'});
     jobctrl.getLink();
     $httpBackend.flush();
     expect(jobctrl.linkApiJob.title).toBe('testing');
+    expect(jobctrl.linkApiJob.url).toBe('www.test.com');
+  });
+
+  it('should change mode on click', () => {
+    jobctrl.jobClick({title:'test click'});
+
+    expect(jobctrl.mode).toBe('single');
+    expect(jobctrl.jobCard.job.title).toBe('test click');
+  });
+
+  it('should add a job to the backlog', () => {
+    $httpBackend.expectPOST('http://localhost:3000/jobs')
+    .respond(200, {title:'testing', company:'123'});
+    jobctrl.addJobs();
+    $httpBackend.flush();
+    expect(jobctrl.backlog[0].title).toBe('testing');
+  });
+
+  it('should update a job', () => {
+    let testJob = {title:'testing', _id:1};
+    let updatedJob = {_id:1, title:'frog'};
+    jobctrl.jobs.push(testJob);
+    $httpBackend.expectPUT('http://localhost:3000/jobs/' + 1)
+    .respond(200);
+    jobctrl.updateJobs(updatedJob);
+    $httpBackend.flush();
+
+    expect(jobctrl.jobs[0].title).toBe('frog');
+    expect(jobctrl.jobs.length).toBe(1);
+  });
+
+  it('should delete a job', () => {
+    let testJob = {title:'test delete', _id:1};
+    $httpBackend.expectDELETE('http://localhost:3000/jobs/' + 1)
+    .respond(200);
+    jobctrl.jobs.push(testJob);
+    jobctrl.deleteJobs(testJob);
+    $httpBackend.flush();
+
+    expect(jobctrl.jobs.length).toBe(0);
   });
 
 });
