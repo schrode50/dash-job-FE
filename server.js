dcb1a3d0037d3729e7849813f7b88a1c7c6d27ef
jdar22169 <mmow313@gmail.com>
2016-07-13 17:04:36 -0700

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 var server = app.listen(process.env.PORT || 3003, function(){
   console.log('server is running at %s', server.address().port);
 });
